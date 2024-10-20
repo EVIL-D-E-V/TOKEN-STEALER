942f1f891bb86618616c350587431c771e0448be
bensgaming <rotoic2@gmail.com>
2024-10-20 19:06:20 +0700

    Add files via upload

---
+++ stub/jscrypter.js
@@ -0,0 +1,18 @@
+var JavaScriptObfuscator = require('javascript-obfuscator');
+var fs = require('fs');
+
+var code = fs.readFileSync(".//node_modules//input.js", "utf8");
+
+var obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
+    renameGlobals: true,
+    compact: false,
+    simplify: true,
+    target: 'node',
+    unicodeEscapeSequence: true
+});
+
+//create the file fn
+
+fs.writeFileSync("..//build//index.js", obfuscationResult.getObfuscatedCode(), "utf8");
+
+console.log("Done Run build.bat to build exe")
